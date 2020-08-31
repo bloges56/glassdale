80ba07afc4e93ec1ae017198d0df2f890e657648
Brady Logan <brady.drew.logan@gmail.com>
2020-08-31 11:54:47 -0500

    styling of criminals section

--- scripts/criminals/Criminal.js
+++ scripts/criminals/Criminal.js
@@ -3,8 +3,18 @@ export const Criminal = (criminal) => {
         <div class="criminal-card">
             <h2>${criminal.name}</h2>
             <div>Age: ${criminal.age}</div>
-            <div>Term start: ${criminal.incarceration.start}</div>
-            <div> Term end: ${criminal.incarceration.end}</div>
+            <div>Crime: ${criminal.conviction}</div>
+            <div>Term start: ${GetFormattedDate(criminal.incarceration.start)}</div>
+            <div> Term end: ${GetFormattedDate(criminal.incarceration.end)}</div>
         </div>
     `;
+}
+
+
+const GetFormattedDate = (date) =>{
+    var todayTime = new Date(date);
+    var month = todayTime.getMonth() + 1;
+    var day = todayTime.getDate();
+    var year = todayTime.getFullYear();
+    return month + "/" + day + "/" + year;
 }
