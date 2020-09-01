bf3b8b51c32b8744db42f8a8247170afb976515d
Brady Logan <brady.drew.logan@gmail.com>
2020-09-01 09:08:24 -0500

    sorted crimes in select component

--- scripts/convictions/ConvictionSelect.js
+++ scripts/convictions/ConvictionSelect.js
@@ -29,7 +29,7 @@ export const ConvictionSelect = () => {
                         return `
                             <option> ${crime.name}</option>
                         `;
-                    }).join("")
+                    }).sort().join("")
                 }
             </select>
         `
