9de12cd6311baf6ca45efb6b2d54a1265567c328
Brady Logan <brady.drew.logan@gmail.com>
2020-09-01 13:47:32 -0500

    added a select officer component that filters criminal list by arresting officer in the dom

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -24,6 +24,25 @@ eventHub.addEventListener('crimeChosen', event => {
     }
 })
 
+eventHub.addEventListener('choseOfficer', event => {
+    // You remembered to add the id of the crime to the event detail, right?
+    if ("name" in event.detail) {
+        /*
+            Filter the criminals application state down to the people that committed the crime
+        */
+        const appStateCriminals = useCriminals();
+        const matchingCriminals = appStateCriminals.filter(criminal => {
+            return criminal.arrestingOfficer === event.detail.name
+        })
+
+        /*
+            Then invoke render() and pass the filtered collection as
+            an argument
+        */
+       render(matchingCriminals);
+    }
+})
+
 const render = criminalCollection => {
     contentTarget.innerHTML = criminalCollection.map(criminal => {
         return Criminal(criminal)
