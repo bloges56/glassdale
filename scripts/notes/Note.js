b05b1eafc99764bd117aa368b6173f09d4b7622b
Brady Logan <brady.drew.logan@gmail.com>
2020-09-17 14:53:19 -0500

    added button to note components that deletes the note

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -2,5 +2,6 @@ export const Note = (note) => {
     return `
         <div>Timestamp: ${note.date}</div>
         <div>${note.note}</div>
+        <button class="noteCriminal--${note.criminalId} "id="deleteNote--${note.id}">Delete Note</button>
     `
 }
